cda6d5f75b40b620818fd1da4e1f011933e06a30
Mahros AL-Qabasy <mahros.elqabasy@gmail.com>
2025-05-09 06:40:30 +0300

    Make gender only Male, Female

--- src/components/register/PersonalInfoFields.tsx
+++ src/components/register/PersonalInfoFields.tsx
@@ -108,8 +108,8 @@ const PersonalInfoFields = ({ formData, handleChange, handleSelectChange }: Pers
             <SelectContent>
               <SelectItem value="male">Male</SelectItem>
               <SelectItem value="female">Female</SelectItem>
-              <SelectItem value="other">Other</SelectItem>
-              <SelectItem value="prefer_not_to_say">Prefer not to say</SelectItem>
+              {/* <SelectItem value="other">Other</SelectItem> */}
+              {/* <SelectItem value="prefer_not_to_say">Prefer not to say</SelectItem> */}
             </SelectContent>
           </Select>
         </div>
